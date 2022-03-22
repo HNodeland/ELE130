95aadf1b16b8a3f650754be2b46eff693d396313
dlqnt <matt.soerensen@gmail.com>
2022-03-22 19:20:24 +0100

    idk

--- Prosjekt04_ManuellKjoring/P04_Plott.py
+++ Prosjekt04_ManuellKjoring/P04_Plott.py
@@ -1,4 +1,6 @@
+#!/usr/bin/env python3
 # coding=utf-8
+
 
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
@@ -28,7 +30,7 @@ EV3_IP = "169.254.200.246"
 # Bruk 'Upload'-funksjonen
 
 # --> Filnavn for lagrede MÅLINGER som skal lastes inn offline
-filenameMeas = "measurements/P04_meas_testkjoring_06.txt"
+filenameMeas = "measurements/P04_meas_mattis_02.txt"
 
 # --> Filnavn for lagring av BEREGNEDE VARIABLE som gjøres offline
 #     Typisk navn:  "CalcOffline_P0X_BeskrivendeTekst_Y.txt"
